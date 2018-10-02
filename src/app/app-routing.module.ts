6edb4e1318f9dc70279d28bb403485b375bf488d
saumye <shavinathsaumye@gmail.com>
2018-10-03 01:27:18 +0530

    configuring routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,26 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { Component } from '../../node_modules/@angular/core';
 
+import { HomeComponent } from './home/home.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { StoreComponent } from './store/store.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+
 const routes: Routes = [
   // { path: '' , component: HomeComponent},
   // { path: 'home', component: HomeComponent },
-  { path: '**', redirectTo: '/home' },
+  { path: '', component: WelcomeComponent },
+  { path: 'home', component: HomeComponent},
+  { path: 'welcome', component: WelcomeComponent },
+  { path: 'store', component: StoreComponent, data: { title: 'storeDetails'}},
+  { path: 'login', component: LoginComponent},
+  { path: 'sign-up', component: SignUpComponent},
+  { path: 'shoping-cart', component: ShoppingCartComponent},
+  { path: '**', redirectTo: '' } 
+
+
 ];
 
 @NgModule({
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeComponent implements OnInit {
 
-  constructor() { }
+  constructor( private router: Router) { }
 
   ngOnInit() {
+  }
+
+  resturantBtn(){
+    this.router.navigateByUrl("home");
+    console.log("resturant clicked");
+  }
+
+  groceryBtn(){
+    this.router.navigateByUrl("home");
+    console.log("grocery clicked");
   }
 
 }
