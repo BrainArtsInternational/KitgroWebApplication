aeea3ba33f4ba9af1e0233a302f7b65b4b42c318
saumye <shavinathsaumye@gmail.com>
2018-10-03 00:27:51 +0530

    adding initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { StoreComponent } from './store/store.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WelcomeComponent,
+    StoreComponent,
+    LoginComponent,
+    SignUpComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
